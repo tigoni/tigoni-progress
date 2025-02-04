f67598c05c397238869771390995df73968723fe
louai <boumedienelouai2@gmail.com>
2025-02-04 07:25:33 +0000

    fix: remove margin left form the github icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
-    <section className="w-screen h-screen flex justify-center items-center bg-background-1">
+    <section className="w-screen h-screen flex px-4 justify-center items-center bg-background-1">
       <div className="w-full md:max-w-3xl gap-y-8 lg:max-w-5xl flex text-center flex-col justify-center items-center">
         <motion.div
           initial={{ opacity: 0, y: 10 }}
@@ -10,7 +10,7 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0 }}
           className="gap-y-4 my-[1vh] md:my-[2vh] flex justify-center items-center flex-col"
         >
-          <div className="size-16 lg:size-20 rounded-full bg-white"></div>
+          <div className="size-20 lg:size-24 rounded-full bg-white"></div>
           <h3 className="text-secondary font-[JetBrains_Mono] text-lg lg:text-2xl font-bold">
             Hi, I'm Louai 👋
           </h3>
@@ -19,7 +19,7 @@ export const Hero = () => {
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.1 }}
-          className="gap-y-4 my-[1vh] flex justify-center items-center flex-col"
+          className="gap-y-2 lg:gap-y-4 lg:my-[1vh] flex justify-center items-center flex-col"
         >
           <h1 className="text-3xl md:text-6xl w-full font-bold tracking-wide text-white">
             Building digital
@@ -46,16 +46,16 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.3 }}
         >
-          <div className="flex flex-col gap-4 sm:flex-row justify-center">
+          <div className="flex flex-col  gap-4 sm:flex-row justify-center">
             <button
-              className="bg-primary text-text-primary font-bold py-2 px-4 rounded-md w-40 lg:w-52 h-10 lg:h-12 hover:bg-primary/80 cursor-pointer transition"
+              className="bg-primary text-text-primary font-bold py-2 px-4 w-64 rounded-md lg:w-52 h-10 lg:h-12 hover:bg-primary/80 cursor-pointer transition"
               onClick={() => console.info("Hello")}
               aria-label="Get started"
             >
               Browse Projects
             </button>
             <button
-              className="bg-background-2 border border-primary/50 text-text-primary font-bold py-2 px-4 rounded-md w-40 lg:w-52 h-10 lg:h-12 cursor-pointer hover:border-primary/90 transition"
+              className="bg-background-2 border border-primary/50 text-text-primary font-bold py-2 px-4 rounded-md w-64 lg:w-52 h-10 lg:h-12 cursor-pointer hover:border-primary/90 transition"
               onClick={() => console.info("Hello")}
               aria-label="Get started"
             >
