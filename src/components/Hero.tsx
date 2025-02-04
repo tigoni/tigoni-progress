e10845aba9f53ee13a8b1786ea25dc37c6bc81b1
louai <boumedienelouai2@gmail.com>
2025-02-04 07:26:56 +0000

    refactor: improve hero section ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,9 +8,9 @@ export const Hero = () => {
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0 }}
-          className="gap-y-4 my-[1vh] md:my-[2vh] flex justify-center items-center flex-col"
+          className="gap-y-8 my-[1vh] md:my-[2vh] flex justify-center items-center flex-col"
         >
-          <div className="size-20 lg:size-24 rounded-full bg-white"></div>
+          <div className="size-20 lg:size-28 rounded-full bg-white"></div>
           <h3 className="text-secondary font-[JetBrains_Mono] text-lg lg:text-2xl font-bold">
             Hi, I'm Louai 👋
           </h3>
@@ -19,7 +19,7 @@ export const Hero = () => {
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.1 }}
-          className="gap-y-2 lg:gap-y-4 lg:my-[1vh] flex justify-center items-center flex-col"
+          className="gap-y-2 lg:gap-y-4 flex justify-center items-center flex-col"
         >
           <h1 className="text-3xl md:text-6xl w-full font-bold tracking-wide text-white">
             Building digital
@@ -36,7 +36,7 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          <h2 className="text-gray-400 text-sm lg:text-lg mx-auto lg:w-2/3">
+          <h2 className="text-text-secondary text-sm lg:text-lg mx-auto lg:w-2/3">
             a Product Designer and Visual Developer in SF. I specialize in UI/UX
             Design, Responsive Web Design, and Visual Development.
           </h2>
@@ -48,14 +48,14 @@ export const Hero = () => {
         >
           <div className="flex flex-col  gap-4 sm:flex-row justify-center">
             <button
-              className="bg-primary text-text-primary font-bold py-2 px-4 w-64 rounded-md lg:w-52 h-10 lg:h-12 hover:bg-primary/80 cursor-pointer transition"
+              className="bg-primary text-text-primary font-bold text-base lg:text-lg w-64 rounded-xl h-10 lg:h-14 hover:bg-primary/80 cursor-pointer transition"
               onClick={() => console.info("Hello")}
               aria-label="Get started"
             >
               Browse Projects
             </button>
             <button
-              className="bg-background-2 border border-primary/50 text-text-primary font-bold py-2 px-4 rounded-md w-64 lg:w-52 h-10 lg:h-12 cursor-pointer hover:border-primary/90 transition"
+              className="bg-background-2 border border-primary/50 text-base lg:text-lg text-text-primary font-bold  rounded-xl w-64  h-10 lg:h-14 cursor-pointer hover:border-primary/90 transition"
               onClick={() => console.info("Hello")}
               aria-label="Get started"
             >
