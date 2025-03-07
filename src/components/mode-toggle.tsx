ecd6beb647717714dabf7cd7b96b5d451174d051
louai <boumedienelouai2@gmail.com>
2025-03-07 17:00:09 +0000

    chore: resetup the project + theme

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/button"
+
+export function ModeToggle() {
+    const [theme, setThemeState] = React.useState<
+        "light" | "dark"
+    >("light")
+
+    React.useEffect(() => {
+        const isDarkMode = document.documentElement.classList.contains("dark")
+        setThemeState(isDarkMode ? "dark" : "light")
+    }, [])
+
+    React.useEffect(() => {
+        const isDark = theme === "dark"
+        document.documentElement.classList[isDark ? "add" : "remove"]("dark")
+    }, [theme])
+
+    const toggleMode = () => {
+        setThemeState(theme === 'dark' ? 'light' : 'dark')
+    }
+
+    return (
+        <Button onClick={toggleMode} variant="ghost" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
+}
