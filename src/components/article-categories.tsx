6d3176625d1468218057feccba529ce376e3a23a
louai <boumedienelouai2@gmail.com>
2025-03-08 10:55:31 +0000

    style: improving styles + fixing some issues on mobile

--- src/components/article-categories.tsx
+++ src/components/article-categories.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils"
 import { useState } from "react"
 import { ArticleItem } from "./article-item"
 import type { Article } from "@/lib/types"
+import { ChevronRight } from "lucide-react"
 
 type ArticleCategoriesProps = {
     articles: Article[]
@@ -13,16 +14,16 @@ export const ArticleCategories = ({ articles }: ArticleCategoriesProps) => {
     const filteredArticles = selected === 'all' ? articles : articles.filter(article => article.category.toLocaleLowerCase() === selected.toLocaleLowerCase())
 
     return (
-        <section className="space-y-3">
-            <h3>Categories</h3>
-            <ul className="w-full flex justify-start items-center gap-x-1">
+        <section className="space-y-3 w-full">
+            <h3 className="text-lg">All articles</h3>
+            <ul className="flex justify-start items-center gap-1 flex-wrap">
                 {
                     [...new Set(['all', ...articles.map(article => article.category)])].map(category => (
-                        <li key={category} className={cn("hover:bg-accent rounded-md py-1 px-6 cursor-pointer duration-150", category === selected && 'bg-accent')} onClick={() => setSelected(category)}>{`${category.charAt(0).toUpperCase()}${category.slice(1)}`}</li>
+                        <li key={category} className={cn("hover:bg-primary hover:text-primary-foreground text-sm rounded-md py-1 px-4 cursor-pointer duration-150", category === selected && 'bg-primary text-primary-foreground')} onClick={() => setSelected(category)}>{`${category.charAt(0).toUpperCase()}${category.slice(1)}`}</li>
                     ))
                 }
             </ul>
-            <div className="flex w-full flex-col justify-start items-start gap-6 mt-8">
+            <div className="flex w-full flex-col justify-start items-start gap-6">
                 {filteredArticles.map((article) => <ArticleItem key={article.id} article={article} />)}
             </div>
         </section>
--- src/components/article-item.tsx
+++ src/components/article-item.tsx
@@ -6,7 +6,7 @@ export const ArticleItem = ({ article }: { article: Article }) => {
     return (
         <a
             href={`/articles/${article.id}`}
-            className="group w-full flex gap-6 justify-start items-center cursor-pointer pb-4 last:border-b-0 border-b"
+            className="group w-full flex gap-6 justify-start items-center duration-75 p-4 rounded-md hover:bg-accent/40 cursor-pointer"
         >
             <img
                 src={article.image}
@@ -48,6 +48,61 @@ export const ArticleItem = ({ article }: { article: Article }) => {
                     </span>
                 </div>
             </div>
+        </a>
+
+    )
+}
+
+
+
+export const FeaturedArticle = ({ featruedArticle }: { featruedArticle: Article }) => {
+
+    return (
+        <a
+            href={`/articles/${featruedArticle.id}`}
+            className="group w-full flex gap-6 justify-start items-center duration-75 p-4 rounded-md hover:bg-accent/40 cursor-pointer"
+        >
+
+            <div className="flex flex-col justify-center gap-4 items-start">
+                <ul className="flex gap-x-4 justify-start items-center">
+                    <li
+                        className="px-3 py-[2px] font-bold rounded-sm bg-secondary text-secondary-foreground text-xs"
+                    >
+                        {
+                            `${featruedArticle.category.charAt(0).toUpperCase()}${featruedArticle.category.slice(1)}`
+                        }
+                    </li>
+                    {
+                        featruedArticle.tags.map((tag) => (
+                            <li key={tag} className="text-sm font-medium">{tag}</li>
+                        ))
+                    }
+                </ul>
+                <div className="space-y-1">
+                    <h3
+                        className="group-hover:underline underline-offset-4 text-2xl font-semibold"
+                    >
+                        {featruedArticle.title}
+                    </h3>
+                    <p className="line-clamp-2 mr-4 text-lg">{featruedArticle.description}</p>
+                </div>
+                <div className="space-y-2 text-xs font-medium">
+                    <p className="text-muted-foreground">
+                        {featruedArticle.date} • {featruedArticle.readTime}
+                    </p>
+                    <span className="items-center flex gap-1">
+                        Read more
+                        <ArrowRight
+                            className="size-3 group-hover:translate-x-0.5 duration-75"
+                        />
+                    </span>
+                </div>
+            </div>
+            <img
+                src={featruedArticle.image}
+                alt={featruedArticle.title}
+                className=" w-[25%]  rounded-md hidden lg:block"
+            />
         </a>
 
     )
