1e00009876d28d03f9fe64e4d3f28b46f0b929d5
louai <boumedienelouai2@gmail.com>
2025-03-14 19:57:05 +0000

    refactor: remove image and updateDate from frontmatter

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,20 +2,18 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/data/blog" }),
-    schema: () =>
-        z.object({
-            pubDatetime: z.date(),
-            modDatetime: z.date().optional().nullable(),
-            series: z.string().optional(),
-            readTime: z.string(),
-            image: z.string(),
-            title: z.string(),
-            draft: z.boolean().optional(),
-            tags: z.array(z.string()).default(["others"]),
-            category: z.string(),
-            description: z.string(),
-        }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/data/blog" }),
+  schema: () =>
+    z.object({
+      pubDatetime: z.date(),
+      series: z.string().optional(),
+      readTime: z.string(),
+      title: z.string(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      category: z.string(),
+      description: z.string(),
+    }),
 });
 
 export const collections = { blog };
