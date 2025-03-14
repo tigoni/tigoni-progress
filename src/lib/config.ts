c1115dae6c88af3b8db7fcd98c2c50731c995fbb
louai <boumedienelouai2@gmail.com>
2025-03-14 23:33:53 +0000

    style: impliment tailwindcss styles

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,7 +7,7 @@ export const SITE = {
   ogImage: "louai-dev-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 4,
+  postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true,
