bb70f99125f2b815300f386e8ca8547d3649814a
louai <boumedienelouai2@gmail.com>
2025-03-21 16:48:19 +0000

    refactor: better experience content + some styling improvements

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -3,9 +3,9 @@ import MarkdownIt from 'markdown-it';
 
 const md = new MarkdownIt();
 
-// Add custom styles for paragraphs and headings
-
 export function markdownToHtml(markdown: string, className?: string): string {
     md.renderer.rules.paragraph_open = () => `<p class="${className}">`;
+
     return md.render(markdown);
 }
+
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,24 +1,11 @@
-export type ExperiencePositionIcon =
-    | "code"
-    | "design"
-    | "education"
-    | "business"
-    | "school";
-
 export type ExperiencePosition = {
-    id: string;
     title: string;
     year: string;
-    employmentType?: string;
-    description?: string;
-    icon?: ExperiencePositionIcon;
-    skills?: string[];
-    expanded?: boolean;
+    description: string;
+    skills: string[];
 };
 
 export type Experience = {
     company: string;
-    companyLogo?: string;
     positions: ExperiencePosition[];
-    current?: boolean;
 };
