293cdae20fa2432f8d0a92a26f57649640b270aa
louai <boumedienelouai2@gmail.com>
2025-03-22 19:44:01 +0000

    fix: add site property to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://louai.site',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
