929b3bbe056d2ac38f1fc4a88d2db289075739c5
louai <boumedienelouai2@gmail.com>
2025-03-22 19:59:54 +0000

    chore: try a remote image for og image

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,8 @@ export const SITE = {
   profile: "https://louai.site",
   desc: "My digital space, projects, insights, and thoughts on software engineering.",
   title: "Louai Boumediene",
-  ogImage: "/louai-og.jpg",
+  // ogImage: "/louai-og.jpg",
+  ogImage: 'https://drive.google.com/file/d/10qMHAiXkC36SDP16eMJQX_tMSGTfHUNA/view?usp=sharing',
   keywords:
     "ncdai, nguyenchanhdai, nguyen chanh dai, chanhdai, chanh dai, iamncdai, quaric, zadark, nguyễn chánh đại, chánh đại",
   postPerPage: 6,
