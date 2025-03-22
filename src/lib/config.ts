34e6954fa1cd227937ca552211db539a8e232489
louai <boumedienelouai2@gmail.com>
2025-03-22 19:20:59 +0000

    fix: og image url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,10 @@
 export const SITE = {
+  pageType: 'website',
   author: "Louai Boumediene",
   profile: "https://louai.site",
   desc: "My digital space, projects, insights, and thoughts on software engineering.",
   title: "Louai Boumediene",
-  ogImage: "louai-og.jpg",
+  ogImage: "/louai-og.jpg",
   keywords:
     "ncdai, nguyenchanhdai, nguyen chanh dai, chanhdai, chanh dai, iamncdai, quaric, zadark, nguyễn chánh đại, chánh đại",
   postPerPage: 6,
