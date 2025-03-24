aab38725dc5c251ea2cd841b2b4010a30854cedc
louai <boumedienelouai2@gmail.com>
2025-03-24 18:45:13 +0000

    feat: add profile picture + add og image

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,9 +4,6 @@ export const SITE = {
   profile: "https://louai.site",
   desc: "My digital space, projects, insights, and thoughts on software engineering.",
   title: "Louai Boumediene",
-  // ogImage: "/louai-og.jpg",
-  ogImage: 'https://drive.google.com/file/d/10qMHAiXkC36SDP16eMJQX_tMSGTfHUNA/view?usp=sharing',
-  keywords:
-    "ncdai, nguyenchanhdai, nguyen chanh dai, chanhdai, chanh dai, iamncdai, quaric, zadark, nguyễn chánh đại, chánh đại",
+  ogImage: '/og-image.jpg',
   postPerPage: 6,
 } as const;
