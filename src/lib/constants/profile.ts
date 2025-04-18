813ddc915b67f7a80b23a89224fdefec12978204
louai <boumedienelouai2@gmail.com>
2025-04-18 16:25:00 +0100

    chore: add email entry to PROFILE info

--- src/lib/constants/profile.ts
+++ src/lib/constants/profile.ts
@@ -3,9 +3,10 @@ export const PROFILE_INFO = {
   logo: 'Louai.b',
   slogan: 'Coding, Creativity, Coffee...',
   displayName: "Louai Boumediene",
+  email: "boumedienelouai2@gmail.com",
   about: `
 I'm a **software engineer** passionate about crafting high-quality digital experiences. With expertise in **full-stack** development and a strong eye for **UI/UX**, I build **performant, pixel-perfect** applications that are both functional and visually appealing.  
 Always **learning**, always **iterating**, always pushing boundaries.  
 `,
-};
+} as const;
 
