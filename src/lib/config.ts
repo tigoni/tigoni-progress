f56ad85883eedd3ad75acabec4ff40020ceb522f
KM <kmuhindi@proton.me>
2025-12-02 22:34:15 +0300

    fix profile pic and update packages

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,6 @@ export const SITE = {
   profile: "https://tigoni.im",
   desc: "“Projects, ideas, and reflections on software engineering and decentralized systems.",
   title: "Ken Kelly | Software Engineer - Web3 & Backend Systems",
-  ogImage: '/og-image.jpg',
+  ogImage: '/profile-pic.jpg',
   postPerPage: 6,
 } as const;
